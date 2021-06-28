1b8b983150df7e5dd79d4569a5ad5831cf40d4a7
Morris Li <limy1912@mails.jlu.edu.cn>
2021-06-28 16:23:00 +0800

    add `while` and `for` loop

--- CH03/loops/src/main.rs
+++ CH03/loops/src/main.rs
@@ -1,5 +1,45 @@
 fn main() {
-    loop {
+    /*loop {
         println!("again!");
+    }*/
+
+    let mut counter = 0;
+
+    /*let result = loop {
+        counter += 1;
+
+        println!("counter = {}", counter);
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    
+    };*/
+
+    while counter < 10 {
+        println!("counter = {}", counter);
+
+        counter += 1;
+    }
+
+    let result = counter * 2;
+
+    println!("The result is {}\n", result);
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+    
+        index += 1;
+
+        println!("the index is: {}", index);
+    }
+
+    println!("---For loop---");
+
+    for element in a.iter() {
+        println!("The value is: {}", element);
     }
 }
