7c475f4f29d30c52507c9408320a25c5438cf4bf
Morris Li <limy1912@mails.jlu.edu.cn>
2021-06-28 15:51:39 +0800

    function with return value

--- CH03/functions/src/main.rs
+++ CH03/functions/src/main.rs
@@ -3,11 +3,14 @@ fn main() {
 
     let x = 5;
 
-    let y = {
+    let _y = {
         let x = 3;
         x + 1
     };
-    another_function(x, y);
+
+    let z = plus_one(five());
+
+    another_function(x, z);
     
     //let x = (let y = 6);
 }
@@ -15,4 +18,12 @@ fn main() {
 fn another_function(x: i32, y: i32) {
     println!("The value of x is: {}.", x);
     println!("The value of y is: {}.", y);
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
