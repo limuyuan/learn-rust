611ad4f49dfd9fc935e772d3e15f24eeb6eac381
Morris Li <limy1912@mails.jlu.edu.cn>
2021-06-28 18:27:08 +0800

    add more lines before and after curly brackets

--- CH04/ownership/src/main.rs
+++ CH04/ownership/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+
+    //println!("the value of s before curly bracket is: {}", s);
+
+    {                      // s is not valid here, it’s not yet declared
+        let s = "hello";   // s is valid from this point forward
+
+        // do stuff with s
+        println!("the value of s in scope is: {}", s);
+    }                      // this scope is now over, and s is no longer valid
+
+    //println!("the value of s after curly bracket is: {}", s);
 }
