7e1e347c3e69b65a58926093f5a0eab71de5badd
Morris Li <limy1912@mails.jlu.edu.cn>
2021-06-28 14:35:49 +0800

    Add return values

--- CH03/functions/src/main.rs
+++ CH03/functions/src/main.rs
@@ -1,7 +1,15 @@
 fn main() {
     println!("Hello, world!");
 
-    another_function(5, 6);
+    let x = 5;
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    another_function(x, y);
+    
+    //let x = (let y = 6);
 }
 
 fn another_function(x: i32, y: i32) {
