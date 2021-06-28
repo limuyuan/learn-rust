e61aece12af5c5790e600b83d0be904f0f20e7f3
Morris Li <limy1912@mails.jlu.edu.cn>
2021-06-28 15:50:52 +0800

    use `if` in a `let` statement

--- CH03/branches/src/main.rs
+++ CH03/branches/src/main.rs
@@ -1,9 +1,17 @@
 fn main() {
-    let number = 3;
+    let number = 6;
 
-    if number < 5 {
+    if number > 7 {
         println!("condition was true");
-    } else {
+    } else if number == 7 {
         println!("condition was false");
+    } else {
+        println!("third arm");
     }
+
+    let condition = true;
+
+    let number = if condition { 5 } else { 6 };
+    
+    println!("The value of number is: {}", number);
 }
