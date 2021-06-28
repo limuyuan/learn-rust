80c89484b592104e3bc0fe19fb11b616994ec3e3
Morris Li <limy1912@mails.jlu.edu.cn>
2021-06-28 11:54:26 +0800

    code of "Invalid Array Element Access"

--- CH03/variables/src/main.rs
+++ CH03/variables/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     //let mut x = 5;
     let x = 5;
@@ -27,4 +29,37 @@ fn main() {
     let one = tup.2;
 
     println!("500: {}, 6.4: {}, 1: {}", five_hundred, six_point_four, one);
+
+    let _months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    let b = [3; 5];
+    let first = b[0];
+    let second = b[1];
+
+    println!("First = {}, second = {}.", first, second);
+
+    println!("------Invalid Array Element Access Codes------");
+
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a numer");
+
+    let element = a[index];
+
+    println!(
+        "The value of the element at index {} is: {}",
+        index, element
+    );
+
 }
