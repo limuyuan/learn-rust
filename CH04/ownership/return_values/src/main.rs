f39fe4220c835d0a23aa3aceb6688e1a379e95f2
Morris Li <limy1912@mails.jlu.edu.cn>
2021-06-29 12:56:50 +0800

    if want to use `s2` again, `s2` must be passed back after pass in

---
+++ CH04/ownership/return_values/src/main.rs
@@ -0,0 +1,35 @@
+fn main() {
+      let s1 = gives_ownership();
+
+      let s2 = String::from("hello");
+
+      let s3 = takes_and_gives_back(s2);
+
+      let (s4, len) = calculate_length(s1);
+
+      println!("The length of '{}' is {}.", s4, len);
+
+      let s2_len = calculate_length_only(s2);
+
+      println!("The length of '{}' is {}.", s2, s2_len);
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("hello");
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+
+    (s, length)
+}
+
+fn calculate_length_only(s: String) -> usize {
+    let length = s.len();
+    length
+}
