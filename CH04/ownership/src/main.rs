0ad7a573bc5421fc6635f8d9c5458a8db8676766
Morris Li <limy1912@mails.jlu.edu.cn>
2021-06-29 10:45:25 +0800

    ownership first work here

--- CH04/ownership/src/main.rs
+++ CH04/ownership/src/main.rs
@@ -17,4 +17,12 @@ fn main() {
     }                      // this scope is now over, and s is no longer valid
 
     //println!("the value of s after curly bracket is: {}", s);
+    code_under_f4_3();
+}
+
+fn code_under_f4_3 () {
+    let s1 = String::from("hello");
+    let s2 = s1;
+
+    println!("{}, world!", s1);
 }
