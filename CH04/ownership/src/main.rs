785ffa20409f35166535319b56dde5efb3da6997
Morris Li <limy1912@mails.jlu.edu.cn>
2021-06-29 10:25:22 +0800

    use `push_str()` method to append literal to a String

--- CH04/ownership/src/main.rs
+++ CH04/ownership/src/main.rs
@@ -3,10 +3,17 @@ fn main() {
     //println!("the value of s before curly bracket is: {}", s);
 
     {                      // s is not valid here, it’s not yet declared
-        let s = "hello";   // s is valid from this point forward
+        //let s = "hello";   // s is valid from this point forward
 
+
+        let mut s = String::from("hello");
         // do stuff with s
+    
+        s.push_str(", world!"); // push_str() appends a literal to a String
+        
         println!("the value of s in scope is: {}", s);
+    
+    
     }                      // this scope is now over, and s is no longer valid
 
     //println!("the value of s after curly bracket is: {}", s);
