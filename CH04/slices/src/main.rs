ed85d94b5b847f204d4208107ec297e697aadbdf
Morris Li <limy1912@mails.jlu.edu.cn>
2021-06-29 18:29:18 +0800

    Listing 4-7

--- CH04/slices/src/main.rs
+++ CH04/slices/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hello, world!");
+
+    let word = String::from("one two");
+    println!("The first word of '{}' is {}.", word, first_word(&word));
+}
+
+fn first_word(s: &String) -> usize {
+    // type of bytes is &[u8]
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
 }
