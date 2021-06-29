46dfc43bd295a0812384c0019980c668f2049917
Morris Li <limy1912@mails.jlu.edu.cn>
2021-06-29 10:56:18 +0800

    no need to use `clone()` for stack-only data

--- CH04/ownership/src/main.rs
+++ CH04/ownership/src/main.rs
@@ -32,5 +32,10 @@ fn clone_string() {
     let s1 = String::from("hello");
     let s2 = s1.clone();
 
+    let x = 5;
+    //let y = x.clone(); no need to use clone() for stack-only data
+    let y = x;
+
     println!("s1 = {}, s2 = {}", s1, s2);
+    println!("x = {}, y = {}", x, y);
 }
