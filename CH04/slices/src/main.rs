fbf0fd8db6ff9d0d61b31a02cd58ac3b21028a26
Morris Li <limy1912@mails.jlu.edu.cn>
2021-06-30 15:27:26 +0800

    add function `second_word`

--- CH04/slices/src/main.rs
+++ CH04/slices/src/main.rs
@@ -1,27 +1,32 @@
+#[allow(unused_mut)]
 fn main() {
     println!("Hello, world!");
 
-    let mut word = String::from("one two");
+    let mut word = String::from("test case");
 
-    let index = first_word(&word);
+    let index = first_word_index(&word);
     
     let one = &word[0..3];
-    let two = &word[4..];
+    let two = &word[3..];
 
     let chinese = String::from("测试");
 
-    let cn1 = &chinese[0..2];
+    let cn1 = &chinese[0..6];
 
     println!("{}, {}", one, two);
 
     println!("{}", cn1);
     
-    word.clear();
+    //word.clear();
 
     println!("The index of first space in word '{}' is {}.", word, index);
+
+    println!("The first word is: {}", first_word(&word));
+
+    println!("The second word is: {}", second_word(&word));
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word_index(s: &String) -> usize {
     // type of bytes is &[u8]
     let bytes = s.as_bytes();
 
@@ -32,4 +37,27 @@ fn first_word(s: &String) -> usize {
     }
 
     s.len()
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn second_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[i+1..];
+        }
+    }
+    return "only 1 word!";
 }
