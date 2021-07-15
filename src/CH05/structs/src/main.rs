076738599b61ea38da3dc42923f057170a901d5e
Morris Li <limy1912@mails.jlu.edu.cn>
2021-07-15 15:17:06 +0800

    struct update syntax and tuple structs

--- src/CH05/structs/src/main.rs
+++ src/CH05/structs/src/main.rs
@@ -1,12 +1,31 @@
 fn main() {
-    let user1 = User {
+    let mut user1 = User {
         email: String::from("someone@example.com"),
         username: String::from("Morris"),
         active: true,
         sign_in_count: 1,
     };
-    
-    println!("{}", user1.username);
+   
+    user1.email = String::from("another@example.com");
+
+
+    println!("{}", user1.email);
+
+
+    let user2 = build_user(String::from("user2@example.com"), String::from("user2"));
+
+    let user3 = User {
+        username: String::from("user3"),
+        email: String::from("user3@3.com"), 
+        ..user2
+    };
+
+    println!("user3's username = {}", user3.username);
+    println!("email of user2: {}, user3: {}", user2.email, user3.email);
+
+    let color = Color(255, 255, 255);
+
+    println!("{}", color.0); 
 }
 
 struct User {
@@ -15,3 +34,14 @@ struct User {
     sign_in_count: u64,
     active: bool,
 }
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
+
+struct Color(i32, i32, i32);
