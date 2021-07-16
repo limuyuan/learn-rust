ebdcc64971d0c69fb7a22f72bcb3213e90aa367d
Morris Li <limy1912@mails.jlu.edu.cn>
2021-07-16 15:42:33 +0800

    refactor with struct

--- src/CH05/rectangles/src/main.rs
+++ src/CH05/rectangles/src/main.rs
@@ -1,3 +1,31 @@
 fn main() {
-    println!("Hello, world!");
+    let width1 = 30;
+    let height1 = 50;
+
+    let rect11 = (30, 50);
+
+    let struct_rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("The area of the rectangle is {} square pixels.", area_by_struct(struct_rect1));
+
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
+
+fn area_by_tuples(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+
+fn area_by_struct(rectangle: Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
+
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
