0d0a6821fca549b8f23604bb52b30984b42645b0
Morris Li <limy1912@mails.jlu.edu.cn>
2021-07-30 11:51:10 +0800

    figure out only heap variants have ownership

--- src/CH06/coins/src/main.rs
+++ src/CH06/coins/src/main.rs
@@ -39,5 +39,41 @@ fn main() {
 
     let number: i32 = 5;
 
-    let sum = another_number / some_number;
+    let test = plus_one_int(some_number);
+    let test2 = plus_one(some_number);
+
+    
+    //let sum = another_number / some_number;
+    println!("5+1={}", plus_one_int(some_number));
+    println!("None=0, so None+1={}", plus_one_int(absent_number));
+
+    println!("{:?}", plus_one(some_number));
+
+    let a_string: Option<String> = Some(String::from("Test"));
+    println!("{:?}", return_string(a_string));
+    //String have ownership
+    //println!("{:?}", return_string(a_string));
+
+
+}
+
+fn plus_one_int(i: Option<i32>) -> i32 {
+    match i {
+        Some(i) => i + 1,
+        None => 0 + 1,
+    }
+}
+
+fn plus_one(i: Option<i32>) -> Option<i32> {
+    match i {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn return_string(s: Option<String>) -> Option<String> {
+    match s {
+        None => None,
+        Some(s) => Some(s),
+    }
 }
