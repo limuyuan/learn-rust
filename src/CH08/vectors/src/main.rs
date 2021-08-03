252000a108fc961d9044e4273877d97bc24f2a1f
Morris Li <limy1912@mails.jlu.edu.cn>
2021-08-03 17:03:02 +0800

    use enum vector

--- src/CH08/vectors/src/main.rs
+++ src/CH08/vectors/src/main.rs
@@ -10,7 +10,42 @@ fn main() {
     v.push(7);
     v.push(9);
 
-    //v.push(1);
+    let _first = &v[0];
 
-    println!("{:?}", v);
+    v.push(10);
+
+    // adding element to end of vec may also relocate full vector, so there's ownership
+    // println!("{:?}", first);
+
+    println!("The second element is: {}", &v[1]);
+    match v.get(2) {
+        None => println!("There's no element"),
+        Some(third) => println!("The third element is: {}", third),
+    }
+
+    for i in &v {
+        println!("{}", i);
+    }
+
+    for i in &mut v {
+        *i += 50;
+    }
+
+    println!("{:#?}", &v);
+
+    #[derive(Debug)]
+    enum SpreadSheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let ev = vec![
+        SpreadSheetCell::Int(12345),
+        SpreadSheetCell::Float(1.234),
+        SpreadSheetCell::Text(String::from("Test")),
+        SpreadSheetCell::Text(String::from("Just a test")),
+    ];
+
+    println!("{:#?}", ev);
 }
