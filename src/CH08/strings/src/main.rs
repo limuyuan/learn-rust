73c846a31280cdedcd3754e40a4901fcf681158f
Morris Li <limy1912@mails.jlu.edu.cn>
2021-08-04 11:41:07 +0800

    use `+` and `format!` to concatenate strings

--- src/CH08/strings/src/main.rs
+++ src/CH08/strings/src/main.rs
@@ -20,4 +20,29 @@ fn main() {
     let hello = String::from("Olá");
     let hello = String::from("Здравствуйте");
     let hello = String::from("Hola");
+
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s2 is {}", s2);
+
+    let mut lo = String::from("lo");
+    lo.push('l');
+    println!("{}", lo);
+
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2; //s1 is moved here so cannot use s1 later
+                       // println!("s1 is {}", s1);
+    println!("s2 is {}, s3 is {}", s2, s3);
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    //let c1 = s1 + "-" + &s2 + "-" + &s3;
+    let c2 = format!("{}-{}-{}", s1, s2, s3);
+
+    println!("{}", c2);
+    println!("{}{}{}", s1, s2, s3); //format! won't take ownership!
 }
