888c0200de4eb10de79f5da74e006a4ea60095bd
Morris Li <limy1912@mails.jlu.edu.cn>
2021-08-05 23:27:07 +0800

    remove useless parenthess, rustfmt done

--- src/CH03/fibonacci/src/main.rs
+++ src/CH03/fibonacci/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
     println!("I put this on leetcode: https://leetcode-cn.com/problems/fibonacci-number/solution/rustfei-bo-na-qi-shu-lie-jie-fa-by-limuy-4y2b/");
 }
+
+struct Solution {}
+
+impl Solution {
+    pub fn fib(n: i32) -> i32 {
+        let mut f_n_2: i32 = 0;
+        let mut f_n_1: i32 = 1;
+        let mut f_n: i32 = 0;
+
+        match n {
+            0 => f_n = 0,
+            1 => f_n = 1,
+            _ => (),
+        }
+
+        for i in 2..n + 1 {
+            f_n = f_n_2 + f_n_1;
+            f_n_2 = f_n_1;
+            f_n_1 = f_n;
+        }
+
+        f_n
+    }
+}
