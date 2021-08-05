1e0c90d8deb3cb73c26ff4776b3a448b5cfed610
Morris Li <limy1912@mails.jlu.edu.cn>
2021-08-05 17:26:24 +0800

    access values in hashmap

--- src/CH08/hashmaps/src/main.rs
+++ src/CH08/hashmaps/src/main.rs
@@ -12,4 +12,19 @@ fn main() {
     let scores1: HashMap<_, _> = teams.into_iter().zip(initial_score.into_iter()).collect();
 
     println!("{:#?}", scores1);
+    
+    let field_name = String::from("Red");
+    let field_value = 0;
+
+    let mut map = HashMap::new();
+
+    map.insert(&field_name, field_value);
+
+    println!("{}, {}, {:?}", field_name, field_value, map);
+
+    println!("{:?}, {:?}", map.get(&field_name), map.get(&String::from("test")));
+    
+    for (key, value) in scores {
+        println!("{}: {}", key, value);
+    }
 }
