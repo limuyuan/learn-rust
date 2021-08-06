1a40440001bf48d0119c97c5348b5f30bf9e859c
Morris Li <limy1912@mails.jlu.edu.cn>
2021-08-06 13:51:21 +0800

    complete lyrcis of "The Twelve Days of Christmas"

---
+++ src/CH03/lyrics/src/main.rs
@@ -0,0 +1,37 @@
+fn main() {
+    let num_order = [
+        "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth",
+        "tenth", "eleventh", "twelfth",
+    ];
+
+    let presents = [
+        "Two turtle doves",
+        "Three French hens",
+        "Four calling birds",
+        "Five gold rings",
+        "Six geese a-laying",
+        "Seven swans a-swimminng",
+        "Eigh maids a-milking",
+        "Nine ladies dancing",
+        "Ten lords a-leaping",
+        "Eleven pipers piping",
+        "Twelve drumers drummingn",
+    ];
+
+    for i in 0..12 {
+        println!(
+            "On the {} day of Christmas my true love sent to me",
+            num_order[i]
+        );
+        match i {
+            0 => println!("A partridge in a pear tree."),
+            _ => {
+                for j in (0..i).rev() {
+                    println!("{},", presents[j]);
+                }
+                println!("And a partridge in a pear tree.");
+            }
+        }
+        println!();
+    }
+}
