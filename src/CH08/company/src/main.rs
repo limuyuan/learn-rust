a3001d72cfb678b8aa6874e6992438b0834eff8d
Morris Li <limy1912@mails.jlu.edu.cn>
2021-08-10 11:17:59 +0800

    exercise 3 complete!

--- src/CH08/company/src/main.rs
+++ src/CH08/company/src/main.rs
@@ -1,35 +1,41 @@
-use std::{io, collections::HashMap};
+use std::{collections::HashMap, io};
 
 fn main() {
     println!("Hello, world!");
-    
-    let company: HashMap<String, String> = HashMap::new();
-    
-    
+
+    let mut company: HashMap<String, String> = HashMap::new();
+
     loop {
         let mut input = String::new();
 
-        println!("Add+[Name]+to+[Dept] or End:");
-    
+        println!("Add+[Name]+to+[Dept] or End|Query:");
+
         io::stdin()
             .read_line(&mut input)
             .expect("Failed to read line!");
 
         let input_vec: Vec<&str> = input.trim().split(" ").collect();
 
-        for s in &input_vec {
-            //println!("{}", s);
-        }
+        //for s in &input_vec {
+        //println!("{}", s);
+        //}
 
         // println!("input_vec[0]{}", input_vec[0]);
 
-        if input_vec[0] == "Add" {
+        if input_vec[0] == "Add" && input_vec.len() == 4 && input_vec[2] == "to" {
+            company.insert(String::from(input_vec[1]), String::from(input_vec[3]));
         } else if input_vec[0] == "End" {
             break;
+        } else if input_vec[0] == "Query" {
+            println!();
+            println!("People in company:");
+            for (name, dept) in &company {
+                println!("{} from {}", name, dept);
+            }
         } else {
-            println!("Invalid input: {}", input_vec[0]);
+            println!("Invalid input: {}", input.trim());
         }
+
+        // println!("company hashmap: {:#?}", company);
     }
 }
-
-fn add_to_dept(name: String) {}
