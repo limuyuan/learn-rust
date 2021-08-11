4a53ed64c4ce94e58d78ac88722f0bd2754af1a4
Morris Li <limy1912@mails.jlu.edu.cn>
2021-08-11 18:09:11 +0800

    closure?

--- src/CH09/panic/src/main.rs
+++ src/CH09/panic/src/main.rs
@@ -1,15 +1,14 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
-    //panic!("Hello, world!");
-
-    //let v = vec![1, 2, 3];
-    // v[99];
-    
-    let f = File::open("test");
-
-    let f = match f {
-        Ok(file) => file,
-        Err(err) => panic!("Problem opening the file: {:#?}", err),
-    };
+    let _f = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
 }
