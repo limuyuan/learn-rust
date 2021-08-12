099e8190f8c7037a12d32e27bd889a5773291fea
Morris Li <limy1912@mails.jlu.edu.cn>
2021-08-12 18:30:01 +0800

    handle error

--- src/CH09/panic/src/main.rs
+++ src/CH09/panic/src/main.rs
@@ -1,7 +1,9 @@
-use std::fs::File;
-use std::io::ErrorKind;
+use std::fs::{self,File};
+use std::io::{self, Read};
+use std::error::Error;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
+    /*
     let _f = File::open("hello.txt").unwrap_or_else(|error| {
         if error.kind() == ErrorKind::NotFound {
             File::create("hello.txt").unwrap_or_else(|error| {
@@ -11,4 +13,44 @@ fn main() {
             panic!("Problem opening the file: {:?}", error);
         }
     });
+    */
+
+    // let f = File::open("hello.txt").unwrap();
+    let _f = File::open("hello.txt")?;
+    Ok(())
+}
+
+/*fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}*/
+
+/*fn second_version() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}*/
+
+/*
+fn third_version() -> Result<String, io::Error> {
+    let mut s = String::new();
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+    Ok(s)
+}
+*/
+
+fn even_shorter() -> Result<String, io::Error> {
+   fs::read_to_string("hello.txt") 
 }
