a364d9b19c110f3bfa0722a757f65cfe0dd3858b
Morris Li <limy1912@mails.jlu.edu.cn>
2021-08-14 01:36:47 +0800

    change `guess` to `Guess` struct

--- src/CH02/guessing_game/src/main.rs
+++ src/CH02/guessing_game/src/main.rs
@@ -17,14 +17,19 @@ fn main() {
             .expect("Failed to read line");
 
         //let guess : u32 = guess.trim().parse().expect("Please type a number!");
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
+        let guess: Guess = match guess.trim().parse() {
+            Ok(num) => Guess::new(num),
             Err(_) => continue,
         };
+/*
+        if guess < 1 || guess > 100 {
+            println!("The secret number will be between 1 and 100.");
+            continue;
+        }
+*/
+        println!("You guessed: {}", guess.value());
 
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
+        match guess.value().cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
@@ -32,5 +37,23 @@ fn main() {
                 break;
             }
         }
+    }
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+        
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
     }
 }
